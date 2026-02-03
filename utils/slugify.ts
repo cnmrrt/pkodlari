dbb303ae21d9068f915690558cd17261dbd664bf
cnmrrt <mertcann.1034@gmail.com>
2026-02-03 14:19:14 +0300

    .

--- utils/slugify.ts
+++
@@ -1,8 +0,0 @@
-export const slugify = (s: string) => {
-    return String(s)
-        .toLocaleLowerCase('tr')
-        .replace(/[ıığüşöç]/g, (m: string) => ({ 'ı': 'i', 'ğ': 'g', 'ü': 'u', 'ş': 's', 'ö': 'o', 'ç': 'c' }[m] || m))
-        .replace(/[^a-z0-9\s-]/g, '')
-        .trim()
-        .replace(/\s+/g, '-');
-};

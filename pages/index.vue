05e960fbe59a50c0ae99f152a41c01fe9fd48576
cnmrrt <mertcann.1034@gmail.com>
2026-02-07 09:35:24 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -53,7 +53,7 @@ class="soft-card p-4 rounded-xl text-center">
 </a>
 <a href="/amasya" class="soft-card p-4 rounded-xl text-center">
 <div class="font-semibold text-slate-900 text-sm truncate">Amasya</div>
-<div class="text-[10px] font-bold text-slate-400 mt-1 uppercase tracking-wider">2 İLÇE </div>
+<div class="text-[10px] font-bold text-slate-400 mt-1 uppercase tracking-wider">3 İLÇE </div>
 </a>
 <a href="/ankara" class="soft-card p-4 rounded-xl text-center">
 <div class="font-semibold text-slate-900 text-sm truncate">Ankara</div>

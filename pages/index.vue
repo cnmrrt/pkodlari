f151dd0a42dae29de7ecd68db839862d2d3de253
cnmrrt <mertcann.1034@gmail.com>
2026-02-13 01:23:03 +0300

    bayburt

--- pages/index.vue
+++ pages/index.vue
@@ -106,7 +106,7 @@ const cityList = computed(() => {
                 <div class="text-[10px] font-bold text-slate-400 mt-1 uppercase tracking-wider">1 İLÇE </div>
             </a><a href="/bayburt" class="soft-card p-4 rounded-xl text-center">
                 <div class="font-semibold text-slate-900 text-sm truncate">Bayburt</div>
-                <div class="text-[10px] font-bold text-slate-400 mt-1 uppercase tracking-wider">1 İLÇE </div>
+                <div class="text-[10px] font-bold text-slate-400 mt-1 uppercase tracking-wider">3 İLÇE </div>
             </a>
 
             <a href="/burdur" class="soft-card p-4 rounded-xl text-center">

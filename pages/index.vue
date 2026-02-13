62c662816b606e28ef4d5600833c7f3ae423eb7f
cnmrrt <mertcann.1034@gmail.com>
2026-02-13 18:41:06 +0300

    artvin

--- pages/index.vue
+++ pages/index.vue
@@ -68,7 +68,7 @@ const cityList = computed(() => {
             </a>
             <a href="/artvin" class="soft-card p-4 rounded-xl text-center">
                 <div class="font-semibold text-slate-900 text-sm truncate">Artvin</div>
-                <div class="text-[10px] font-bold text-slate-400 mt-1 uppercase tracking-wider">1 İLÇE </div>
+                <div class="text-[10px] font-bold text-slate-400 mt-1 uppercase tracking-wider">9 İLÇE </div>
             </a>
             <a href="/aydin" class="soft-card p-4 rounded-xl text-center">
                 <div class="font-semibold text-slate-900 text-sm truncate">Aydın</div>

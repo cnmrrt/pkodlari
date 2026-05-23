9b7538188990eb1e6cf321b7b1e20bebfb483335
cnmrrt <mertcann.1034@gmail.com>
2026-05-23 23:16:22 +0300

    .

--- pages/erzurum/[district]/index.vue
+++ pages/erzurum/[district]/index.vue
@@ -7,7 +7,7 @@ const route = useRoute() ;
 const filter = ref('');
 
 const { data: erzurumData } = await useAsyncData('erzurum-data', () =>
-$fetch<any[]>('https://words-from-life-5cb26-default-rtdb.firebaseio.com/posta_kodlari_duzenlenmis/erzurum.json')
+$fetch<any[]>('https://pkodlari.com/data/erzurum.json')
 )                                                                                                                 ;
 
 const districtSlug = computed(() => route.params.district as string) ;
--- pages/erzurum/index.vue
+++ pages/erzurum/index.vue
@@ -3,7 +3,7 @@ import { ArrowLeft, ChevronRight } from 'lucide-vue-next';
 import { slugify, titleCase } from '~/utils/slugify';
 
 const { data: erzurumData } = await useAsyncData('erzurum-data', () =>
-$fetch<any[]>('https://words-from-life-5cb26-default-rtdb.firebaseio.com/posta_kodlari_duzenlenmis/erzurum.json')
+$fetch<any[]>('https://pkodlari.com/data/erzurum.json')
 )                                                                                                                 ;
 
 const districts = computed(() => {

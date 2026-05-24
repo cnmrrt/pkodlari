c20cc61805ffba0abf79b3b174439f2056ed1e0f
cnmrrt <mertcann.1034@gmail.com>
2026-05-24 20:16:05 +0300

    .

--- pages/artvin/[district]/[neighborhood].vue
+++ pages/artvin/[district]/[neighborhood].vue
@@ -7,7 +7,7 @@ const route = useRoute()  ;
 const copied = ref(false) ;
 
 const { data: artvinData } = await useAsyncData('artvin-data', () =>
-$fetch<any[]>('https://words-from-life-5cb26-default-rtdb.firebaseio.com/posta_kodlari_duzenlenmis/artvin.json')
+$fetch<any[]>('https://pkodlari.com/data/artvin.json')
 )                                                                                                                ;
 
 const districtSlug = computed(() => route.params.district as string)         ;

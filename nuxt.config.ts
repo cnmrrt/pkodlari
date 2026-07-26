e078d2c6576efe3b18632970792e06d5040ba309
cnmrrt <mertcann.1034@gmail.com>
2026-07-27 02:39:05 +0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
     app: {
-        baseURL: '/pkodlari/',
         head: {
             htmlAttrs: {
                 lang: 'tr'

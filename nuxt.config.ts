35ec74d188c18a892f8fbcd62bd33787712d03b4
cnmrrt <mertcann.1034@gmail.com>
2026-07-26 20:42:25 +0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
     app: {
-        baseURL: '/',
         head: {
             htmlAttrs: {
                 lang: 'tr'
@@ -58,8 +57,6 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
             ]
         }
     },
-    nitro: {
-        preset: 'static'
-    },
+
     ssr: true
 })

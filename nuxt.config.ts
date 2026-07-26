cd87cf874aea6e7f2370eedc180adb1064169b89
cnmrrt <mertcann.1034@gmail.com>
2026-07-26 20:35:55 +0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
     devtools: { enabled: true },
     app: {
+        baseURL: '/',
         head: {
             htmlAttrs: {
                 lang: 'tr'
@@ -56,6 +57,9 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
                 }
             ]
         }
+    },
+    nitro: {
+        preset: 'static'
     },
     ssr: true
 })

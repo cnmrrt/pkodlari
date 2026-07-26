8ccbe5955ff0fc19d3b17360eb95efd966c8c889
cnmrrt <mertcann.1034@gmail.com>
2026-07-27 00:05:45 +0300

    Update index.vue

--- pages/[city]/index.vue
+++ pages/[city]/index.vue
@@ -100,6 +100,8 @@ const isValid = computed(() => !!cityItem.value);
 </script>
 
 <template> 
+
+
   <div v-if="isValid" class="animate-in fade-in duration-500 max-w-4xl mx-auto">
     <div class="mb-10 flex items-center gap-4">
       <NuxtLink to="/" class="text-slate-400 hover:text-slate-900 transition-colors"
